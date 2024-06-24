4fadca4bb6e274ef75471d0e42bb39bbc39a1c6d
Ben <Benbhoy1888@gmail.com>
2024-06-24 16:57:26 +0100

    New solution

---
+++ 30 Days of Code/Day3/IntroToConditionalStatements.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        if (N >=1 && N <=100)
+        {
+            switch(N % 2)
+            {
+                case 0:
+                    if (N >=2 && N <=5)
+                    {
+                        Console.WriteLine("Not Weird");
+                        break;  
+                    }
+                    
+                    else if (N >=6 && N <=20)
+                    {
+                        Console.WriteLine("Weird");
+                        break;  
+                    }
+                    
+                    else if (N > 20)
+                    {
+                        Console.WriteLine("Not Weird");
+                        break;  
+                    }
+                    
+                    break;
+                   
+                case 1:
+                    Console.WriteLine("Weird");
+                    break;
+            }
+        }
+        
+    }
+}
